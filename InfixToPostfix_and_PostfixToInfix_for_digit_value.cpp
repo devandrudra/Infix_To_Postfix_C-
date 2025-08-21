0b213ed5f3427acef8c8c13bb040528921e512eb
devandrudra <rudradevmishra3@gmail.com>
2025-08-21 12:19:07 +0530

    Infix_to_postfix_and_compute_value_for_real_one

--- InfixToPostfix_and_PostfixToInfix_for_digit_value.cpp
+++ InfixToPostfix_and_PostfixToInfix_for_digit_value.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 #include<stack>
+#include<math.h>
 
 
 int precedence(char ch)
@@ -57,12 +58,66 @@ string InfixToPostfix(string infix)
     return postfix;
 }
 
+int operation(int operand1, int operand2, char op)
+{
+    switch (op)
+    {
+    case '*':
+        return operand2 * operand1;
+        break;
+    case '/':
+        return operand2 / operand1;
+        break;
+    
+    case '+':
+        return operand2 + operand1;
+        break;
+
+    case '-':
+        return operand2 - operand1;
+        break;
+
+    case '^':
+        return pow(operand2,operand1);
+        break;
+    
+    default:
+        return 0;
+    }
+}
+
+int PostfixToInfix(string postfix)
+{
+    stack<int> Stack;
+    int result = 0;
+    for(char ch : postfix)
+    {
+        if(ch >= '0' && ch <= '9')
+        {
+            Stack.push(int(ch - '0'));
+        }
+        else
+        {
+            int operand1 = Stack.top();
+            Stack.pop();
+            int operand2 = Stack.top();
+            Stack.pop();
+            Stack.push(operation(operand1, operand2, ch));
+            
+        }
+    }
+    return Stack.top();
+}
+
 int main()
 {
 
-    string infix = "K+L-M*N+(O^P)*W/U/V*T+Q"; // KL+MN*-OP^W*U/V/T*+Q+
-    string exp = "a+b*(c^d-e)^(f+g*h)-i"; // abcd^e-fgh*+^*+i-
+    string infix = "2+4-6*7+(2^5-7)"; //"K+L-M*N+(O^P)*W/U/V*T+Q"; // KL+MN*-OP^W*U/V/T*+Q+
+    // string exp =  //"a+b*(c^d-e)^(f+g*h)-i"; // abcd^e-fgh*+^*+i-
     string postfix = InfixToPostfix(infix);
     cout << postfix;
+    cout << endl << PostfixToInfix(postfix);
+    
+    // cout << endl << operation('4', '5', '^');
     return 0;
 }
